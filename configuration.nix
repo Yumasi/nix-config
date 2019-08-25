52c7d0041616ea83eb1b15af70a4d8070bcc1c05
Guillaume Pagnoux <guillaume.pagnoux@lse.epita.fr>
2019-08-25 14:18:43 +0000

    Add Virtualbox config
    Signed-off-by: Guillaume Pagnoux <guillaume.pagnoux@lse.epita.fr>

---
+++ configuration.nix
@@ -0,0 +1,29 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  services.virtualbox.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    vim tmux git mosh htop
+  ];
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
+
+}
