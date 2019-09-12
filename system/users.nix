544e67831943235fc275c867306af104a70d5bc9
Guillaume Pagnoux <guillaume.pagnoux@epita.fr>
2019-09-12 19:34:07 +0200

    system: make my default shell zsh
    Signed-off-by: Guillaume Pagnoux <guillaume.pagnoux@epita.fr>

--- system/users.nix
+++ system/users.nix
@@ -1,12 +1,15 @@
 { config, pkgs, libs, ... }:
 {
   users.groups = { yumasi = { }; };
+  environment.pathsToLink = [ "/share/zsh" ];
 
-  users.users.yumasi = {
+  users.users.yumasi = with pkgs; {
     description = "Guillaume Pagnoux";
     home = "/home/yumasi";
     group = "yumasi";
     extraGroups = [ "libvirtd" "users" "video" "wheel" ];
     isNormalUser = true;
+    shell = zsh;
+    uid = 1000;
   };
 }
