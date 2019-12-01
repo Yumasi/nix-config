0121ca3d1668e5fb74479cc27fb8b163098f1b83
Guillaume Pagnoux <guillaume.pagnoux@lse.epita.fr>
2019-12-01 23:27:22 +0100

    flavours: add home flavour
    Signed-off-by: Guillaume Pagnoux <guillaume.pagnoux@lse.epita.fr>

---
+++ flavours/home.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, lib, ... }:
+{
+  imports = [
+    ../features/ccache.nix
+    ../features/virt.nix
+    ../features/weechat.nix
+    ../features/x11.nix
+  ];
+
+  # I don't care about mitigations in a VM
+  boot.kernelParams = [ "mitigations=off" ];
+
+  hardware = {
+    pulseaudio.enable = true;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+}
