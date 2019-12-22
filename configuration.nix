7ef2b207420b153a0963c4c04e4790c9d878990c
Guillaume Pagnoux <guillaume.pagnoux@lse.epita.fr>
2019-12-22 04:08:02 +0100

    global: use latest kernel
    Signed-off-by: Guillaume Pagnoux <guillaume.pagnoux@lse.epita.fr>

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,7 @@ in
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = hostName;
 
